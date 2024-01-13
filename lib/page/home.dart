6d6ad2f4d9e94be105419b7d043076db4d3a7426
Naveen Akash <naveen.akash0904@gmail.com>
2024-01-13 17:05:52 +0530

    Rough design of course tile

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -18,61 +18,46 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       drawer: const Drawer(),
-      body: const Column(
-        children: [
-          Row(
-            children: [
-              CourseTile(
-                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                gradientColors: [
-                  Color.fromARGB(255, 127, 198, 255),
-                  Color.fromARGB(255, 185, 144, 255),
-                ],
-                title: "Introduction",
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              CourseTile(
-                margin: EdgeInsets.fromLTRB(20, 10, 10, 10),
-                gradientColors: [
-                  Color.fromARGB(255, 255, 197, 110),
-                  Color.fromARGB(255, 255, 121, 111),
-                ],
-                title: "Section 1",
-              ),
-              CourseTile(
-                margin: EdgeInsets.fromLTRB(10, 10, 20, 10),
-                gradientColors: [
-                  Color.fromARGB(255, 133, 255, 137),
-                  Color.fromARGB(255, 243, 255, 133),
-                ],
-                title: "Section 2",
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              CourseTile(
-                margin: EdgeInsets.fromLTRB(20, 10, 10, 10),
-                gradientColors: [
-                  Color.fromARGB(255, 255, 123, 123),
-                  Color.fromARGB(255, 255, 170, 206),
-                ],
-                title: "Section 3",
-              ),
-              CourseTile(
-                margin: EdgeInsets.fromLTRB(10, 10, 20, 10),
-                gradientColors: [
-                  Color.fromARGB(255, 148, 207, 255),
-                  Color.fromARGB(255, 150, 255, 206),
-                ],
-                title: "Section 4",
-              ),
-            ],
-          ),
-        ],
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            CourseTile(
+              gradientColors: [
+                Color.fromARGB(255, 127, 198, 255),
+                Color.fromARGB(255, 185, 144, 255),
+              ],
+              title: "Introduction",
+            ),
+            CourseTile(
+              gradientColors: [
+                Color.fromARGB(255, 255, 197, 110),
+                Color.fromARGB(255, 255, 121, 111),
+              ],
+              title: "Section 1",
+            ),
+            CourseTile(
+              gradientColors: [
+                Color.fromARGB(255, 133, 255, 137),
+                Color.fromARGB(255, 243, 255, 133),
+              ],
+              title: "Section 2",
+            ),
+            CourseTile(
+              gradientColors: [
+                Color.fromARGB(255, 255, 123, 123),
+                Color.fromARGB(255, 255, 170, 206),
+              ],
+              title: "Section 3",
+            ),
+            CourseTile(
+              gradientColors: [
+                Color.fromARGB(255, 148, 207, 255),
+                Color.fromARGB(255, 150, 255, 206),
+              ],
+              title: "Section 4",
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/course_tile.dart
+++ lib/widgets/course_tile.dart
@@ -4,54 +4,135 @@ class CourseTile extends StatelessWidget {
   const CourseTile({
     required this.gradientColors,
     required this.title,
-    this.margin,
     super.key,
   });
 
   final List<Color> gradientColors;
   final String title;
-  final EdgeInsets? margin;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: margin,
-        child: InkWell(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(20.0),
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      width: double.infinity,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(255, 227, 150, 255),
+            blurRadius: 1,
+            offset: Offset(5, 5),
           ),
-          onTap: () {
-            print('Container tapped!');
-          },
-          child: Ink(
-            width: double.infinity,
-            height: 175,
-            padding: const EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: gradientColors,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
+        ],
+      ),
+      child: Stack(
+        children: [
+          Image.asset(
+            "assets/images/doodad.jpg",
+            fit: BoxFit.cover,
+          ),
+          Column(
+            children: [
+              const SizedBox(height: 60),
+              Image.asset(
+                "assets/images/splash.png",
+                width: 100,
+                height: 100,
               ),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(20.0),
+              const SizedBox(height: 20),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 241, 241, 241),
+                    ),
               ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        fontWeight: FontWeight.w700,
-                      ),
+              Text(
+                "Lorem Ipsum sdnsdlk jnknknknkn nd typesettingindustry.",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: const Color.fromARGB(210, 255, 255, 255),
+                    ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 30,
+            right: 40,
+            child: Material(
+              color: Colors.transparent,
+              child: Ink(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              ],
+                child: InkWell(
+                  onTap: () {
+                    // Handle the click event here
+                  },
+                  borderRadius: BorderRadius.circular(30),
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                    child: Text(
+                      "Play",
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: const Color.fromARGB(255, 255, 110, 7),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
+
+    // return Container(
+    //   margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+    //   child: InkWell(
+    //     borderRadius: const BorderRadius.all(
+    //       Radius.circular(20.0),
+    //     ),
+    //     onTap: () {
+    //       print('Container tapped!');
+    //     },
+    //     child: Ink(
+    //       width: double.infinity,
+    //       height: 325,
+    //       padding: const EdgeInsets.all(30),
+    //       decoration: BoxDecoration(
+    //         gradient: LinearGradient(
+    //           colors: gradientColors,
+    //           begin: Alignment.topLeft,
+    //           end: Alignment.bottomRight,
+    //         ),
+    //         borderRadius: const BorderRadius.all(
+    //           Radius.circular(20.0),
+    //         ),
+    //       ),
+    //       child: Stack(
+    //         children: [
+    //           Positioned(
+    //             width: 100,
+    //             height: 100,
+    //             child: Image.asset("assets/images/splash.png",),
+    //           ),
+    //           Positioned(
+    //             child: Text(
+    //               title,
+    //               style: Theme.of(context).textTheme.titleLarge!.copyWith(
+    //                     fontWeight: FontWeight.w700,
+    //                   ),
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
